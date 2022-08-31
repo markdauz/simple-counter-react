f71daa331e736146275999a1eded39ac8d4a8676
markdauz <87331275+markdauz@users.noreply.github.com>
2022-08-31 18:35:57 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [number, setNumber] = useState(0);
+  return (
+    <div className="App">
+      <h1>Increment/Decrement Counter</h1>
+      <h2>Counter : {number}</h2>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <div
+          style={{
+            cursor: "pointer",
+            width: 20,
+            padding: 8,
+            margin: 8,
+            backgroundColor: "gray"
+          }}
+          onClick={() => setNumber((prev) => prev + 1)}
+        >
+          +
+        </div>
+        <div
+          onClick={() => setNumber((prev) => prev + 1)}
+          style={{
+            cursor: "pointer",
+            width: 20,
+            padding: 8,
+            margin: 8,
+            backgroundColor: "gray"
+          }}
+        >
+          -
+        </div>
+      </div>
+    </div>
+  );
+}
